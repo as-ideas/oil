14bcbfd461519d28b2953162de612e139829e498
Andreas Wagner <andreas.wagner@axelspringer.com>
2017-04-04 15:37:11 +0200

    Add overlay to index

--- src/oil.js
+++ src/oil.js
@@ -1,1 +1,34 @@
-console.log('42');
+require("./oil.scss");
+
+const defineOilContent = () => {
+  return (
+    `
+      <h1 class="oil__heading">
+        Axel Springer Optin Layer
+      </h1>
+      <p class="oil__intro-text">
+        Hi, this is a little intro text.
+      </p>
+      <button class="oil__button oil__button--primary">
+        Yes, I want!
+      </button>
+      <button class="oil__button oil__button--secondary">
+        Not now
+      </button>
+    `
+  );
+};
+
+const injectOil = (entryPoint) => {
+  const oil = document.createElement("div");
+  oil.setAttribute('class', 'oil');
+  oil.innerHTML = defineOilContent();
+  entryPoint.appendChild(oil);
+};
+
+injectOil(document.body);
+
+
+
+
+
