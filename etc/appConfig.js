60aa03bc81aa965bfb8e0f1abfaf39157b82a75b
Martin Reinhardt <contact@martinreinhardt-online.de>
2017-04-04 11:36:39 +0200

    chore(build): Clean up app config

--- etc/appConfig.js
+++ etc/appConfig.js
@@ -4,30 +4,31 @@ const sourcePath = 'src';
 const testPath = 'test';
 const outputPath = 'dist  ';
 
-module.exports = appConfig = {
-  appName: 'OptInLayer',
-  srcPath: sourcePath,
-  testPath: testPath,
-  junit: {
-    title: 'OptInLayer',
-    dir: outputPath + '/test-reports'
-  },
-  templatesPath: 'templates',
-  distPath: outputPath,
-  globals: {},
-  chunks: {
-    name: ['polyfills'].reverse()
-  },
-  indexFiles: [{
-    template: path.resolve(sourcePath, 'index.html'),
-    chunks: ['app', 'polyfills'],
-    chunksSortMode: 'dependency'
-  }],
-  entry: {
-    app: path.resolve(sourcePath, 'oil.js')
-  },
-  mangle: {},
-  proxy: {},
-  title: 'OIL',
-  additionalWebpackOptions: false
+
+module.exports = {
+    appName: 'OptInLayer',
+    srcPath: sourcePath,
+    testPath: testPath,
+    junit: {
+        title: 'OptInLayer',
+        dir: outputPath + '/test-reports'
+    },
+    templatesPath: 'templates',
+    distPath: outputPath,
+    globals: {},
+    chunks: {
+        name: ['polyfills'].reverse()
+    },
+    indexFiles: [{
+        template: path.resolve(sourcePath, 'index.html'),
+        chunks: ['app', 'polyfills'],
+        chunksSortMode: 'dependency'
+    }],
+    entry: {
+        app: path.resolve(sourcePath, 'oil.js')
+    },
+    mangle: {},
+    proxy: {},
+    title: 'OIL',
+    additionalWebpackOptions: false
 };
