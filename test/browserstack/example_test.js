0ab1c1f49f44f0a499f7b0c713e385dc6cc2acdc
Marcel Bankmann <mbankman@asideas.de>
2017-04-10 12:48:55 +0200

    fixed missing basic auth login in nightwatch test

--- test/browserstack/example_test.js
+++ test/browserstack/example_test.js
@@ -1,10 +1,10 @@
 module.exports = {
   'OIL Integration Online' : function (browser) {
     browser
-      .url('https://oil-integration.herokuapp.com/')
+      .url('https://oil:rig@oil-integration.herokuapp.com/')
       .waitForElementVisible('body', 1000)
-      // .pause(1000)
-      // .assert.title('OIL - Aktuelles')
+      .pause(1000)
+      .assert.title('OIL - Aktuelles')
       .end();
   }
 };
