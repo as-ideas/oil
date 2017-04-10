235d19eef3135d8f03c04d6b2bcf26178d4b68a5
Marcel Bankmann <mbankman@asideas.de>
2017-04-10 12:36:35 +0200

    added nightwatch testing

--- test/browserstack/example_test.js
+++ test/browserstack/example_test.js
@@ -1,23 +1,11 @@
-// var webdriver = require('selenium-webdriver');
-
-// // Input capabilities
-// var capabilities = {
-//   'browserName' : 'firefox', 
-//   'browserstack.user' : 'marcelbankmann1',
-//   'browserstack.key' : 'YsD5Lq6KuGg2oxk9Sscx'
-// }
-
-// var driver = new webdriver.Builder().
-//   usingServer('http://hub-cloud.browserstack.com/wd/hub').
-//   withCapabilities(capabilities).
-//   build();
-
-// driver.get('http://www.google.com');
-// driver.findElement(webdriver.By.name('q')).sendKeys('BrowserStack');
-// driver.findElement(webdriver.By.name('btnG')).click();
-
-// driver.getTitle().then(function(title) {
-//   console.log(title);
-// });
-
-// driver.quit();
+module.exports = {
+  'Google\'s Search Functionality' : function (browser) {
+    browser
+      .url('https://www.google.com/ncr')
+      .waitForElementVisible('body', 1000)
+      .setValue('input[type=text]', 'BrowserStack\n')
+      .pause(1000)
+      .assert.title('BrowserStack - Google Search')
+      .end();
+  }
+};
