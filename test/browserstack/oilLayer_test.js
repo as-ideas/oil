a1619fdde1cf69524334a8ad197ef1254bf6c42f
Marcel Bankmann <mbankman@asideas.de>
2017-04-12 10:25:45 +0200

    added safari 10 workaround for second test as well

--- test/browserstack/oilLayer_test.js
+++ test/browserstack/oilLayer_test.js
@@ -2,7 +2,7 @@ module.exports = {
   'OIL Layer Visible' : function (browser) {
     browser
       .url('https://oil:rig@oil-integration.herokuapp.com/')
-      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('body', 1000, false)
       .useXpath()
       .assert.visible('//*[@data-qa="oilLayer"]')
       .end();
