12e643e726031d86b977c4fe5e9ff2ecb1e1b473
Martin Reinhardt <contact@martinreinhardt-online.de>
2017-04-18 12:39:01 +0200

    refactor(persist-optin): Split optin to module

--- src/oil.js
+++ src/oil.js
@@ -1,17 +1,8 @@
 import "./oil.scss";
-import Cookie from 'js-cookie';
-import { isDOMElement, addClickHandler, isCookie, isCookieValid } from './scripts/utils.js';
+import { isDOMElement, addClickHandler } from './scripts/utils.js';
 import { findConfiguration } from "./scripts/config";
+import { getOilCookie, validateOilCookie, oilOptIn, oilOptLater } from "./scripts/optin";
 
-// Our cookie default settings
-const oilCookie = {
-  name: 'oil_data',
-  expires: 31,
-  config: {
-    optin: false,
-    expanded: true
-  }
-}
 
 
 /**
@@ -42,25 +33,25 @@ function defineOilContent() {
  */
 function injectOil(entryPoint) {
   // Cookie should be present...
-  const cookieData = Cookie.getJSON(oilCookie.name);
+  let cookieData = getOilCookie();
 
   if (typeof(cookieData) !== 'undefined' && isDOMElement(entryPoint)) {
     // Create overlay container
-    const oil = document.createElement('div');
-    
-    // Add classes for styling Oil overlay 
+    let oil = document.createElement('div');
+
+    // Add classes for styling Oil overlay
     // Classes are added sequentially, see here for problems in some browsers with alternative syntax oil.classList.add('foo', 'bar', 'baz')
-    // http://stackoverflow.com/questions/11115998/is-there-a-way-to-add-remove-several-classes-in-one-single-instruction-with-clas 
+    // http://stackoverflow.com/questions/11115998/is-there-a-way-to-add-remove-several-classes-in-one-single-instruction-with-clas
     oil.classList.add(`oil`)
     oil.classList.add(`optin-${cookieData.optin}`)
     oil.classList.add(`expanded-${cookieData.expanded}`)
 
     // Add data attribute for testing purposes
     oil.setAttribute('data-qa', 'oilLayer' );
-    
+
     // Add overlay content
     oil.innerHTML = defineOilContent();
-    
+
     // Add to DOM
     entryPoint.appendChild(oil);
   }
@@ -71,8 +62,8 @@ function injectOil(entryPoint) {
  * Update Oil overlay class names
  */
 function updateOilOverlay(dataObj) {
-  const oilOverlay = document.getElementsByClassName('oil')[0]
-  
+  let oilOverlay = document.getElementsByClassName('oil')[0]
+
   // Reset classlist to Oil base class
   oilOverlay.setAttribute('class', 'oil')
 
@@ -82,58 +73,16 @@ function updateOilOverlay(dataObj) {
 }
 
 
-/**
- * Oil optIn
- */
-function oilOptIn() {
-  // Cookie should be there...
-  const cookieData = Cookie.getJSON(oilCookie.name)
-  const newCookieData = Object.assign({}, cookieData, {optin: true});
-  
-  // Update Oil cookie
-  Cookie.set(oilCookie.name, newCookieData, {
-    expires: oilCookie.expires
-  });
-
-  // Update classes on Oil overlay DOM element
-  updateOilOverlay(newCookieData)
-  
-  console.log("User opted in, cookie set")
-}
-
-
-/**
- * Oil optLater
- */
-function oilOptLater() {
-  // Cookie should be there...
-  const cookieData = Cookie.getJSON(oilCookie.name)
-  const newCookieData = Object.assign({}, cookieData, {expanded: false});
-
-  // Update Oil cookie
-  Cookie.set(oilCookie.name, newCookieData, {
-    expires: oilCookie.expires
-  });
-
-  // Update classes on Oil overlay DOM element
-  updateOilOverlay(newCookieData)
-
-  console.log("User opted later, cookie set")
-}
 
 
 /**
  * Add click handler
  */
 function addOilClickHandler() {
-  const btnOptIn = document.getElementsByClassName('js-optin')[0];
-  const btnOptLater = document.getElementsByClassName('js-optlater')[0];
-  addClickHandler(btnOptIn, () => {
-    oilOptIn();
-  });
-  addClickHandler(btnOptLater, () => {
-    oilOptLater()
-  });
+  let btnOptIn = document.getElementsByClassName('js-optin')[0],
+      btnOptLater = document.getElementsByClassName('js-optlater')[0];
+  addClickHandler(btnOptIn, () => oilOptIn().then((cookieData) => updateOilOverlay(cookieData)));
+  addClickHandler(btnOptLater, () => oilOptLater().then((cookieData) => updateOilOverlay(cookieData)));
 }
 
 
@@ -141,15 +90,8 @@ function addOilClickHandler() {
  * Init OIL
  */
 (function(){
-  // Set Oil cookie if no cookie exists
-  if (!isCookie(oilCookie.name)) {
-    Cookie.set(oilCookie.name, oilCookie.config, {expires: oilCookie.expires})
-  }
 
-  // In case Oil cookie exists but is not valid, create new Oil cookie with default config 
-  if (!isCookieValid(oilCookie.name, Object.keys(oilCookie.config))) {
-    Cookie.set(oilCookie.name, oilCookie.config, {expires: oilCookie.expires})
-  }
+  validateOilCookie();
 
   // Inject Oil overlay depending on cookie data
   injectOil(document.body);
---
+++ src/scripts/optin.js
@@ -0,0 +1,76 @@
+import Cookie from 'js-cookie';
+import { isCookie, isCookieValid } from './utils.js';
+
+// Our cookie default settings
+const oilCookie = {
+  name: 'oil_data',
+  expires: 31,
+  config: {
+    optin: false,
+    expanded: true
+  }
+}
+
+export function validateOilCookie() {
+  // Set Oil cookie if no cookie exists
+  if (!isCookie(oilCookie.name)) {
+    setDefaultOilCookie();
+  }
+
+  // In case Oil cookie exists but is not valid, create new Oil cookie with default config
+  if (!isCookieValid(oilCookie.name, Object.keys(oilCookie.config))) {
+    setDefaultOilCookie();
+  }
+}
+
+
+export function getOilCookie() {
+  return Cookie.getJSON(oilCookie.name);
+}
+
+export function setDefaultOilCookie() {
+  return Cookie.set(oilCookie.name, oilCookie.config, {expires: oilCookie.expires});
+}
+
+/**
+ * Oil optIn
+ * @return promise with updated cookie value
+ */
+export function oilOptIn() {
+  // Cookie should be there...
+  const cookieData = Cookie.getJSON(oilCookie.name)
+  const newCookieData = Object.assign({}, cookieData, {optin: true});
+
+  // Update Oil cookie
+  Cookie.set(oilCookie.name, newCookieData, {
+    expires: oilCookie.expires
+  });
+
+  // Update classes on Oil overlay DOM element
+  return new Promise((resolve) => {
+    resolve(newCookieData);
+    console.log("User opted in, cookie set");
+  });
+}
+
+
+/**
+ * Oil optLater
+ * @return promise with updated cookie value
+ */
+export function oilOptLater() {
+  // Cookie should be there...
+  const cookieData = Cookie.getJSON(oilCookie.name)
+  const newCookieData = Object.assign({}, cookieData, {expanded: false});
+
+  // Update Oil cookie
+  Cookie.set(oilCookie.name, newCookieData, {
+    expires: oilCookie.expires
+  });
+
+  // Update classes on Oil overlay DOM element
+  return new Promise((resolve) => {
+    resolve(newCookieData);
+    console.log("User opted later, cookie set");
+  });
+}
