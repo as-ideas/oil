a7f8e85f3d2264ba71a58cf33905db56222c93a5
Marcel Bankmann <mbankman@asideas.de>
2017-04-27 18:43:09 +0200

    testing removing all logs alltogether

--- src/scripts/log.js
+++ src/scripts/log.js
@@ -1,19 +1,19 @@
 import { isProd } from "./utils";
 
 export function logInfo() {
-  if (console && console.log) {
-    console.log(console, arguments);
-  }
+  // if (console && console.log) {
+  //   console.log(console, arguments);
+  // }
 }
 
 export function logError() {
-  if (console && console.log) {
-    console.log(console, arguments);
-  }
+  // if (console && console.log) {
+  //   console.log(console, arguments);
+  // }
 }
 
 export function logDebug() {
-  if (!isProd() && console && console.log) {
-    console.log(console, arguments);
-  }
+  // if (!isProd() && console && console.log) {
+  //   console.log(console, arguments);
+  // }
 }
