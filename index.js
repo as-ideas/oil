58877ae4d4e7e700f7d4eac6e2803606b96c6f53
Reinhardt, Martin <null>
2017-05-03 12:28:29 +0200

    refactor(logging): Use improved logging

--- index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  appConfig: require('./etc/appConfig.js'),
-  karma: require('./etc/karma.conf.js'),
-  karma_ci: require('./etc/karma.conf.ci.js'),
-  webpack: require('./etc/webpack.config.js')
-};
--- src/scripts/log.js
+++ src/scripts/log.js
@@ -1,22 +1,37 @@
 import { isProd } from "./utils";
 
-export function initLogging() {
-  if (!window.console) window.console = {};
-  if (!window.console.log) window.console.log = function () { };
-}
-
-export function logInfo() {
-  if (!isProd()) {
-    console.log(console, arguments);
+export function logError() {
+  if (!isProd() && console && console.error) {
+    try {
+      console.error.apply(console, arguments);
+    }
+    catch (e) {
+      var log = Function.prototype.bind.call(console.log, console);
+      log.apply(console, arguments);
+    }
   }
 }
 
-export function logError() {
-    console.log(console, arguments);
+export function logInfo() {
+  if (!isProd() && console && console.info) {
+    try {
+    console.info.apply(console, arguments);
+    }
+    catch (e) {
+      var log = Function.prototype.bind.call(console.log, console);
+      log.apply(console, arguments);
+    }
+  }
 }
 
 export function logDebug() {
-  if (!isProd()) {
-    console.log(console, arguments);
+  if (!isProd() && console && console.debug) {
+    try {
+    console.debug.apply(console, arguments);
+    }
+    catch (e) {
+      var log = Function.prototype.bind.call(console.log, console);
+      log.apply(console, arguments);
+    }
   }
 }
