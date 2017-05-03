66eb1a6b61600fb31d9fac7e71faa903aed113ee
Marcel Bankmann <mbankman@asideas.de>
2017-05-03 14:27:13 +0200

    skipping some less interesting environments for now

--- etc/runBrowserstackSeq.sh
+++ etc/runBrowserstackSeq.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 echo "- Chrome Recent -----------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e chromeRecent
+# ./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e chromeRecent
 echo "- IE9 ---------------------------------------------------------------------"
 ./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e ie9
 echo "- Firefox Recent ----------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e firefoxRecent
+# ./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e firefoxRecent
 echo "- Safari Recent -----------------------------------------------------------"
 ./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e safariRecent
 echo "- Android 4 ---------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e android4
+# ./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e android4
 echo "- iPhone 5 ----------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e iphone5
+# ./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e iphone5
