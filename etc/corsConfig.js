e98fc7fd03330c238c3a874dca929a61148cf2e5
Reinhardt, Martin <null>
2017-05-04 10:48:34 +0200

    chore(build): Move CORS config to own module
    can now be used in webpack dev and http server

---
+++ etc/corsConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization, x-id, Content-Length, X-Requested-With",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS"
+  }
+}
