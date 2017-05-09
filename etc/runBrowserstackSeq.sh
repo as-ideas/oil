0f214cb029f9c634731248c3b00e74a35fee25c9
Marcel Bankmann <mbankman@asideas.de>
2017-05-09 17:52:50 +0200

    test only on safari, fixed env

--- etc/runBrowserstackSeq.sh
+++ etc/runBrowserstackSeq.sh
@@ -1,14 +1,14 @@
 #/bin/bash
 echo "- Chrome Recent -----------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e chromeRecent
+#./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e chromeRecent
 echo "- Firefox Recent ----------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e firefoxRecent
+#./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e firefoxRecent
 echo "- IE9 ---------------------------------------------------------------------"
 # ./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e ie9
 echo "- Firefox Recent ----------------------------------------------------------"
 # ./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e firefoxRecent
 echo "- Safari91 ----------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e Safari91
+./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e safariRecent
 echo "- Android 4 ---------------------------------------------------------------"
 # ./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e android4
 echo "- iPhone 5 ----------------------------------------------------------------"
