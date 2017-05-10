734a1c86517a8c8e570e8856c11bca633a5403f4
Reinhardt, Martin <null>
2017-05-10 07:54:53 +0200

    fix(CORS): Fix MSIE CORS setup

--- etc/corsConfig.js
+++ etc/corsConfig.js
@@ -3,7 +3,8 @@ let headers = {
   "Access-Control-Allow-Origin": "http://localhost:8080 | http://localhost:3000 | http://localhost:9001 | http://oilcdn:8080 | http://oilsite1:8080 | http://oilsite2:8080 | http://oilcdn:3000 | http://oilsite1:3000 | http://oilsite2:3000 | http://oil-integration-cdn.herokuapp.com | https://oil-integration-cdn.herokuapp.com",
   "Access-Control-Allow-Credentials": "true",
   "Access-Control-Allow-Headers": "Content-Type, Authorization, x-id, Content-Length, X-Requested-With, Origin",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS"
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "p3p": "CP=\"IDC DSP COR ADM DEVi TAIi PSA PSD IVAi IVDi CONi HIS OUR IND CNT\""
   // end::cors-config[]
 };
 module.exports = {
