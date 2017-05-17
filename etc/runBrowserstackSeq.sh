09912f67a3d863fa86c486b900e9bd5316ab2abf
Marcel Bankmann <mbankman@asideas.de>
2017-05-17 12:19:42 +0200

    removed cors, test p3p header

--- etc/runBrowserstackSeq.sh
+++ etc/runBrowserstackSeq.sh
@@ -1,14 +1,14 @@
 #/bin/bash
 echo "- Chrome 57 ---------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e chrome57
+# ./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e chrome57
 echo "- IE9 ---------------------------------------------------------------------"
 ./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e ie9
 echo "- Firefox 9 ---------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e ff9
+# ./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e ff9
 echo "- Firefox 52 --------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e ff52
+# ./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e ff52
 echo "- Safari91 ----------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e safari91
+# ./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e safari91
 echo "- iPhone 5 ----------------------------------------------------------------"
 # ./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e iphone5 # fails weirdly, check later
 #echo "- Android 4 ---------------------------------------------------------------"
