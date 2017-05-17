9121c5240ed198df0c6e656edd7366c6613c03f5
Marcel Bankmann <mbankman@asideas.de>
2017-05-17 11:36:33 +0200

    removed cors for checking validity without it

--- etc/corsConfig.js
+++ etc/corsConfig.js
@@ -1,12 +1,13 @@
 let headers = {
   // tag::cors-config[]
-  "Access-Control-Allow-Origin": "http://localhost:8080 | http://localhost:3000 | http://localhost:9001 | http://oilcdn:8080 | http://oilsite1:8080 | http://oilsite2:8080 | http://oilcdn:3000 | http://oilsite1:3000 | http://oilsite2:3000 | http://oil-integration-cdn.herokuapp.com | https://oil-integration-cdn.herokuapp.com",
-  "Access-Control-Allow-Credentials": "true",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization, x-id, Content-Length, X-Requested-With, Origin",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "p3p": "CP=\"IDC DSP COR ADM DEVi TAIi PSA PSD IVAi IVDi CONi HIS OUR IND CNT\""
   // end::cors-config[]
 };
 module.exports = {
   headers: headers
 };
+
+// "Access-Control-Allow-Origin": "http://localhost:8080 | http://localhost:3000 | http://localhost:9001 | http://oilcdn:8080 | http://oilsite1:8080 | http://oilsite2:8080 | http://oilcdn:3000 | http://oilsite1:3000 | http://oilsite2:3000 | http://oil-integration-cdn.herokuapp.com | https://oil-integration-cdn.herokuapp.com",
+// "Access-Control-Allow-Credentials": "true",
+// "Access-Control-Allow-Headers": "Content-Type, Authorization, x-id, Content-Length, X-Requested-With, Origin",
+// "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
