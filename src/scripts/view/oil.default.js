8dc2673f3fbeb393f782b97f2fa7be70ad03bd01
Andreas Wagner <andreas.wagner@axelspringer.com>
2017-06-30 11:46:32 +0200

    Update CSS layout, text for no cookie template, fix logging, supress logging in production mode

--- src/scripts/view/oil.default.js
+++ src/scripts/view/oil.default.js
@@ -3,23 +3,25 @@ import { oilHeading, oilIntroText, CSSPrefix} from './oil.view.config.js';
 
 export const oilDefaultTemplate = `
     <div class="${CSSPrefix}oil-content-overlay ${CSSPrefix}oil-has-gradient" data-qa="oil-full">
-        <h1 class="${CSSPrefix}oil__heading">
-            ${oilHeading()}
-        </h1>
-        <p class="${CSSPrefix}oil__intro-txt">
-            ${oilIntroText()}
-        </p>
-        <div class="${CSSPrefix}oil-l-row">
-            ${POIButtonSnippet()}
-            <div class="${CSSPrefix}oil-l-item">
-                <button class="${CSSPrefix}oil__btn-soi js-optin" data-qa="oil-YesButton" onClick="gaTrackEvent('SOI/yes');">
-                    Jetzt zustimmen
-                </button>
-            </div>
-            <div class="${CSSPrefix}oil-l-item ${CSSPrefix}oil-l-item--stretch">
-                <button class="${CSSPrefix}oil__btn-loi js-optlater" data-qa="oil-NotNowButton">
-                    Nein, jetzt nicht
-                </button>
+        <div class="${CSSPrefix}oil-l-wrapper-layout-max-width">
+            <h1 class="${CSSPrefix}oil__heading">
+                ${oilHeading()}
+            </h1>
+            <p class="${CSSPrefix}oil__intro-txt">
+                ${oilIntroText()}
+            </p>
+            <div class="${CSSPrefix}oil-l-row">
+                ${POIButtonSnippet()}
+                <div class="${CSSPrefix}oil-l-item">
+                    <button class="${CSSPrefix}oil__btn-soi js-optin" data-qa="oil-YesButton" onClick="gaTrackEvent('SOI/yes');">
+                        Jetzt zustimmen
+                    </button>
+                </div>
+                <div class="${CSSPrefix}oil-l-item ${CSSPrefix}oil-l-item--stretch">
+                    <button class="${CSSPrefix}oil__btn-loi js-optlater" data-qa="oil-NotNowButton">
+                        Nein, jetzt nicht
+                    </button>
+                </div>
             </div>
         </div>
     </div>
--- src/scripts/view/oil.no.cookies.js
+++ src/scripts/view/oil.no.cookies.js
@@ -2,11 +2,26 @@ import { CSSPrefix} from './oil.view.config.js';
 
 export const oilNoCookiesTemplate = `
     <div class="${CSSPrefix}oil-content-overlay" data-qa="oil-nocookies">
-        <h1 class="${CSSPrefix}oil__heading">
-            Um unsere Dienste für Sie noch besser zu machen, brauchen wir Cookies.
-        </h1>
-        <p class="${CSSPrefix}oil__intro-text">
-            Geben Sie uns ihre Cookies, jetzt!
-        </p>
+        <div class="${CSSPrefix}oil-l-wrapper-layout-max-width">
+            <h1 class="${CSSPrefix}oil__heading">
+                Um unsere Services bestmöglich erbringen zu können, müssen in deinem Browser Cookies aktiviert sein.
+            </h1>
+            <p class="${CSSPrefix}oil__intro-txt">
+                Bitte aktiviere Cookies in den Einstellungen deines Browsers. 
+                So kannst du in 
+                <a href="https://support.google.com/chrome/answer/95647?co=GENIE.Platform%3DDesktop&hl=en-GB" 
+                    class="${CSSPrefix}oil__intro-txt--link" 
+                    target="_blank"
+                >
+                    Google Chrome
+                </a>oder 
+                <a href="https://support.mozilla.org/en-US/kb/cookies-information-websites-store-on-your-computer" 
+                    class="${CSSPrefix}oil__intro-txt--link" 
+                    target="_blank"
+                >
+                    Firefox
+                </a>Cookies aktivieren. 
+            </p>
+        </div>
     </div>
 `;
