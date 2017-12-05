2fa50060e17f64ae4a5c91afb186baf9e1cfa599
Bankmann, Marcel <marcel.bankmann@asideas.de>
2017-12-05 12:53:23 +0100

    http_server pro fix, immonet round 3

--- http_server.js
+++ http_server.js
@@ -74,9 +74,11 @@ app.all(allowCrossDomain);
 // Serve directory indexes folder (with icons)
 app.use('/release', serveIndex('release', {'icons': true}));
 app.use('/examples', serveIndex('dist/examples', {'icons': true}));
+app.use('/devExamples', serveIndex('src/examples', {'icons': true}));
 
 // static with cache headers
 app.use(serveStatic(DOCUMENT_ROOT, {maxAge: CACHE_DURATION, cacheControl: true}));
+app.use('/devExamples', express.static('src/examples'))
 
 console.log('server is now starting on port ', port);
 app.listen(port, '0.0.0.0');
