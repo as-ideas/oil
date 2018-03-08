b499590eb9d1c4f7e1a7a379f428d9e9898e256e
Awerkow-Schäfer, Dmitri <dmitri.awerkow@asideas.de>
2018-03-08 11:34:21 +0100

    no-ticket: added new language pack

--- src/scripts/userview/locale/userview_oil_enEN_01.js
+++ src/scripts/userview/locale/userview_oil_enEN_01.js
@@ -12,13 +12,13 @@ window.AS_OIL_LOCALE = {
   [OIL_LABELS.ATTR_PRIVACY_PAGE_URL]: undefined,
   [OIL_LABELS.ATTR_LABEL_INTRO_HEADING]: 'We use cookies and other technologies',
   [OIL_LABELS.ATTR_LABEL_INTRO]: undefined,
-  [OIL_LABELS.ATTR_LABEL_INTRO_START]:'Die Website verwendet Cookies, Web Beacons, JavaScript und ähnliche Technologien. Bitte stimmen Sie zu, dass die <a href="#" class="as-oil__intro-txt--link as-js-companyList">Konzerngesellschaften</a>  der Axel Springer SE und <a href="#" class="as-oil__intro-txt--link">ausgewählte andere Unternehmen</a> für die bedarfsgerechte Gestaltung, für Marktforschung oder für Werbung Nutzungsprofile bei Verwendung von Pseudonymen erstellen und diese an Dritte weitergegeben. Diese Nutzungsprofile dürfen nicht mit Daten über den Träger des Pseudonyms zusammengeführt werden. Detaillierte Informationen und Hinweise zu Ihrem Widerspruchsrecht finden Sie in der <a href="//oil.asideas.de/legal/thirdparties.html" class="as-oil__intro-txt--link" target="_blank">Datenschutzerklärung</a>.',
+  [OIL_LABELS.ATTR_LABEL_INTRO_START]:'The website uses cookies, web beacons, JavaScript and similar technologies. I agree that <a href="#" class="as-oil__intro-txt--link as-js-companyList">companies belonging to Axel Springer SE</a> and <a href="//oil.asideas.de/legal/thirdparties.html" target="_blank" class="as-oil__intro-txt--link as-js-companyList">trusted partners</a> generate pseudonymous user profiles for adapting the website to the user, for market research and for advertising. The generated data can also be shared with third parties while the user profiles cannot be combined with personal data. Detailed information, also on the right to withdraw consent, can be found in the website\'s privacy policy.',
   [OIL_LABELS.ATTR_LABEL_INTRO_END]: '',
   [OIL_LABELS.ATTR_LABEL_BUTTON_YES]: 'OK',
-  [OIL_LABELS.ATTR_LABEL_BUTTON_NO]: 'Nein, jetzt nicht',
-  [OIL_LABELS.ATTR_LABEL_BUTTON_BACK]: 'Zurück',
-  [OIL_LABELS.ATTR_LABEL_BUTTON_PRIVACY]: 'Mehr erfahren',
-  [OIL_LABELS.ATTR_LABEL_BUTTON_ADVANCED_SETTINGS]: 'Mehr Informationen',
+  [OIL_LABELS.ATTR_LABEL_BUTTON_NO]: 'NO, ',
+  [OIL_LABELS.ATTR_LABEL_BUTTON_BACK]: 'Back',
+  [OIL_LABELS.ATTR_LABEL_BUTTON_PRIVACY]: 'Learn more',
+  [OIL_LABELS.ATTR_LABEL_BUTTON_ADVANCED_SETTINGS]: 'More information',
   [OIL_LABELS.ATTR_LABEL_ADVANCED_SETTINGS_HEADING]: 'Bitte wähle eine Datenschutzeinstellung aus:',
   [OIL_LABELS.ATTR_LABEL_ADVANCED_SETTINGS_TEXT]: '',
   [OIL_LABELS.ATTR_LABEL_ADVANCED_SETTINGS_ESSENTIAL_TEXT]: 'Nur erforderliche Cookies',
@@ -30,5 +30,5 @@ window.AS_OIL_LOCALE = {
   [OIL_LABELS.ATTR_LABEL_POI_GROUP_LIST_HEADING]: 'Ihre Einwilligung für Konzerngesellschaften',
   [OIL_LABELS.ATTR_LABEL_POI_GROUP_LIST_TEXT]: 'Hier ist eine Liste von Konzerngesellschaften:',
   [OIL_LABELS.ATTR_LABEL_NO_COOKIES_HEADING]: 'Um unsere Services bestmöglich erbringen zu können, müssen in deinem Browser Cookies aktiviert sein.',
-  [OIL_LABELS.ATTR_LABEL_NO_COOKIES_TEXT]: 'Bitte aktiviere Cookies in den Einstellungen deines Browsers. So kannst du in  <a href="https://support.google.com/chrome/answer/95647?co=GENIE.Platform%3DDesktop&hl=en-GB" class="as-oil__intro-txt--link" target="_blank" > Google Chrome </a>oder <a href="https://support.mozilla.org/en-US/kb/cookies-information-websites-store-on-your-computer" class="as-oil__intro-txt--link" target="_blank" > Firefox </a>Cookies aktivieren.'
+  [OIL_LABELS.ATTR_LABEL_NO_COOKIES_TEXT]: 'Please activate Cookies in the properties of your Browsers. So you can do it in <a href="https://support.google.com/chrome/answer/95647?co=GENIE.Platform%3DDesktop&hl=en-GB" class="as-oil__intro-txt--link" target="_blank" > Google Chrome </a>or <a href="https://support.mozilla.org/en-US/kb/cookies-information-websites-store-on-your-computer" class="as-oil__intro-txt--link" target="_blank" > Firefox </a>.'
 };
