7d0b9be0504bc1e8929044e23f88ba916bb39a30
nnitzsch <nina-marie.nitzsche@asideas.de>
2018-03-14 13:10:58 +0100

    add thirdPartyList

---
+++ src/scripts/third-party-list/poi-group/poi-group_axelSpringerSe_01.js
@@ -0,0 +1,34 @@
+
+export {
+  oilThirdPartyListTemplate
+} from '../oil.3rdParties.list';
+
+export const thirdParties = [
+  '@Leisure BR BV',
+  'AWIN AG',
+  'Axel Springer Mediahouse Berlin GmbH',
+  'Axel Springer SE',
+  'B.Z. Ullstein GmbH',
+  'Bonial International GmbH',
+  'CASAMUNDO GmbH',
+  'COMPUTER BILD Digital GmbH',
+  'Contact Impact GmbH',
+  'DanCenter A/S',
+  'finanzen.net GmbH',
+  'idealo internet GmbH',
+  'Immonet GmbH',
+  'Immosolve GmbH',
+  'Immowelt AG',
+  'infoRoad GmbH',
+  'meinestadt GmbH',
+  'MeinProspekt GmbH',
+  'StepStone Deutschland GmbH / Stepstone Group',
+  'Transfermarkt GmbH & Co. KG',
+  'Traum-Ferienwohnungen GmbH',
+  'Umzugsauktion GmbH & Co. KG',
+  'upday GmbH & Co. KG',
+  'Vertical Media GmbH',
+  'Visoon Video Impact GmbH & Co. KG',
+  'Visual Meta GmbH',
+  'YOURCAREERGROUP GmbH'
+];
