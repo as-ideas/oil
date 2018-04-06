a2ea6b6d35852d650823bf8d342cbf549affad83
Sebastian Waschnick <sebastian.waschnick@asideas.de>
2018-04-06 13:40:00 +0200

    Adding version to docs and docs to releases #2

--- docs/asciidoc.js
+++ docs/asciidoc.js
@@ -2,12 +2,15 @@ const glob = require('glob');
 const asciiDoctor = require('asciidoctor.js')();
 const fs = require('fs');
 const base_dir = require('path').resolve();
+const Path = require('path');
 
-if (!fs.existsSync(base_dir.concat('/dist/docs/'))){
+if (!fs.existsSync(base_dir.concat('/dist/docs/'))) {
   fs.mkdirSync(base_dir.concat('/dist/docs/'));
 }
 
 const BUNDLE_VERSION = process.env.npm_package_version + (process.env.SNAPSHOT || '-SNAPSHOT');
+const SUPPORTED_LANGUAGE = getSupportedLanguagesFromDir();
+const SUPPORTED_POI_GROUPS = getSupportedPoiGroupsFromDir();
 
 glob('docs/*.adoc', function (err, files) {
   files.forEach(function (file) {
@@ -18,14 +21,35 @@ glob('docs/*.adoc', function (err, files) {
       'to_dir': base_dir.concat('/dist/docs/'),
       'base_dir': base_dir.concat('/docs/'),
       'attributes': {
-        'version': BUNDLE_VERSION
+        'version': BUNDLE_VERSION,
+        'SUPPORTED_LANGUAGE': SUPPORTED_LANGUAGE,
+        'SUPPORTED_POI_GROUPS': SUPPORTED_POI_GROUPS
       }
     });
   });
 });
 
+function getSupportedLanguagesFromDir() {
+  return getFilenames(base_dir.concat('/src/scripts/userview/locale/'))
+    .map((item) => {
+      return ('<li>' + item + '</li>').replace('userview_oil_', '').replace('.js', '');
+    }).join(' ');
+}
 
+function getSupportedPoiGroupsFromDir() {
+  return getFilenames(base_dir.concat('/src/scripts/poi-list/lists/'))
+    .map((item) => {
+      return ('<li>' + item + '</li>').replace('poi-info_', '').replace('.js', '');
+    }).join(' ');
+}
 
+function getFilenames(path) {
+  return fs.readdirSync(path)
+    .filter((item) => {
+      return fs.statSync(Path.join(path, item)).isFile()
+    })
+    .sort()
+}
 
 
 
