3779ee83a5d0dd2adc6d90de1445ff74dbf73cd1
Alexander Glintschert <alexander.glintschert@asideas.de>
2018-05-03 18:29:15 +0200

    OIL-73: implement getVendorConsents (wip)

--- src/scripts/core/core_consents.js
+++ src/scripts/core/core_consents.js
@@ -1,29 +1,56 @@
 import {getSoiCookie} from './core_cookies';
+import {getPurposeList} from './core_vendor_information';
+import {encodeCookieValue, encodeIntToBits} from './core_utils';
 
 export function getVendorConsentData(vendorIds) {
-  let base64EncodedMetaData = '';
-  let purposeConsents = getPurposeConsents();
-  let vendorConsents = {};
-
-
   return {
-    metadata: base64EncodedMetaData,
-    gdprApplies: false,
+    metadata: encodeCookieValue(buildMetaData()),
+    gdprApplies: true,
     hasGlobalScope: false,
-    purposeConsents: purposeConsents,
-    vendorConsents: vendorConsents
+    purposeConsents: buildPurposeConsents(),
+    vendorConsents: buildVendorConsents(vendorIds)
   };
 }
 
-function getPurposeConsents() {
+function buildPurposeConsents() {
   let soiCookie = getSoiCookie();
   let privacy = soiCookie.privacy;
 
   if (typeof privacy === 'object') {
-
+    return soiCookie.privacy;
   } else {
-    // purposeConsents =
+    let purposes = getPurposeList();
+    let numberOfPurposes = purposes.length;
+    let purposeConsents = {};
+
+    for (let i = 0; i < numberOfPurposes; i++) {
+      purposeConsents[purposes[i].id] = privacy;
+    }
+    return purposeConsents;
   }
-  // return purposeConsents;
-  return {};
 }
+
+function buildVendorConsents(vendorIds) {
+  let soiCookie = getSoiCookie();
+  let numberOfVendors = vendorIds.length;
+  let vendorConsents = {};
+
+  for (let i = 0; i < numberOfVendors; i++) {
+    vendorConsents[vendorIds[i]] = soiCookie.opt_in;
+  }
+  return vendorConsents;
+}
+
+function buildMetaData() {
+  let soiCookie = getSoiCookie();
+  let cookieVersionAsBitString = encodeIntToBits(1, 6);
+  let creationTimeAsBitString = encodeIntToBits(Math.round(soiCookie.timestamp / 100), 36); // TODO where do we get the creation time from?
+  let lastUpdateTimeAsBitString = encodeIntToBits(Math.round(soiCookie.timestamp / 100), 36);
+  let cmpIdAsBitString = encodeIntToBits(1, 12); // TODO what is our CMP Id?
+  let cmpVersionAsBitString = encodeIntToBits(1, 6); // TODO We need a numeric OIL version here.
+  let consentScreenAsBitString = encodeIntToBits(1, 6);
+  let vendorListVersionAsBitString = encodeIntToBits(1, 12); // TODO Currently, we don't get the vendor list dynamically!
+
+  return `${cookieVersionAsBitString}${creationTimeAsBitString}${lastUpdateTimeAsBitString}${cmpIdAsBitString}${cmpVersionAsBitString}${consentScreenAsBitString}${vendorListVersionAsBitString}`;
+}
+
