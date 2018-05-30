9a47c983fa526ada98b174035ea712d8718276cb
Dominique Klevinghaus <adk@codekommando.de>
2018-05-30 10:51:24 +0200

    Refactor browserstack shell scripts to take in user and key input

--- etc/runFullBrowserstackSeq.sh
+++ etc/runFullBrowserstackSeq.sh
@@ -1,64 +1,25 @@
 #/bin/bash
 
-echo "- Chrome 14 --------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e chrome14
+source etc/browserstackTest.sh
 
-echo "- Chrome 40 --------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e chrome40
-
-echo "- Chrome 57 --------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e chrome57
-
-echo "- IE8 --------------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e ie8
-
-echo "- IE9 --------------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e ie9
-
-echo "- IE10 --------------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e ie10
-
-echo "- IE11 --------------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e ie11
-
-echo "- Edge -------------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e edge14
-
-echo "- Firefox 9 --------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e ff9
-
-echo "- Firefox 35 -------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e ff35
-
-echo "- Firefox 52 -------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e ff52
-
-echo "- Opera 12.16 ------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e opera1216
-
-echo "- Safari 6 ---------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e safari6
-
-echo "- Safari 71 --------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e safari71
-
-echo "- Safari 8 ---------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e safari8
-
-echo "- Safari91 ---------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e safari91
-
-echo "- iPhone 5 ---------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e iphone5
-
-echo "- iPad 4 -----------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e ipad4
-
-echo "- iPhone 6 ---------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e iphone6
-
-echo "- Android 4 --------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e android4
-
-echo "- Android 5 --------------------------------------------------------------------"
-./node_modules/.bin/nightwatch -c etc/nightwatch.remote.conf.js -e android5
+run_test chrome14
+run_test chrome40
+run_test chrome57
+run_test ie8
+run_test ie9
+run_test ie10
+run_test ie11
+run_test edge14
+run_test ff9
+run_test ff35
+run_test ff52
+run_test opera1216
+run_test safari6
+run_test safari71
+run_test safari8
+run_test safari91
+run_test iphone5
+run_test ipad4
+run_test iphone6
+run_test android4
+run_test android5
