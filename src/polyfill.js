a68b4ef42bb9e7685ce547eb1851946b6392f5c1
Dominique Klevinghaus <adk@codekommando.de>
2018-06-19 22:37:09 +0200

    Add a few more polyfills

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,10 +1,48 @@
-export function polyfillPromise(){
+export function generatePolyfills(){
   if(typeof window === 'undefined' || !window.Promise) {
     // eslint-disable-next-line global-require
-    window.Promise = require('core-js/fn/promise');
-  } else {
-    return null;
+    window.Promise = require('promise-polyfill').default;
   }
+
+  // eslint-disable-next-line global-require
+  require('es6-object-assign/auto');
+
+  if (!Array.prototype.fill) {
+    console.log('prototype fill');
+    // eslint-disable-next-line no-extend-native
+    Object.defineProperty(Array.prototype, 'fill', {
+      value: function(value) {
+
+        if (this === null) {
+          throw new TypeError('this is null or not defined');
+        }
+
+        let O = Object(this);
+        let len = O.length >>> 0;
+        let start = arguments[1];
+        let relativeStart = start >> 0;
+        let k = relativeStart < 0 ?
+          Math.max(len + relativeStart, 0) :
+          Math.min(relativeStart, len);
+
+        let end = arguments[2];
+        let relativeEnd = end === undefined ?
+          len : end >> 0;
+
+        let final = relativeEnd < 0 ?
+          Math.max(len + relativeEnd, 0) :
+          Math.min(relativeEnd, len);
+
+        while (k < final) {
+          O[k] = value;
+          k++;
+        }
+
+        return O;
+      }
+    });
+  }
+  console.log(Array.prototype.fill);
 }
 
-export default polyfillPromise();
+export default generatePolyfills();
