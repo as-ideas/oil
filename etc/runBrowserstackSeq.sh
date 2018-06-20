1e2102c12c69691186195494ffcc552d5718609b
Dominique Klevinghaus <adk@codekommando.de>
2018-06-20 20:54:50 +0200

    Uncomment relevant profiles

--- etc/runBrowserstackSeq.sh
+++ etc/runBrowserstackSeq.sh
@@ -2,10 +2,10 @@
 
 source etc/browserstackTest.sh
 
-# run_test chrome57
+run_test chrome57
 run_test ie10
-# run_test ff58quantum
-# run_test safari91
+run_test ff58quantum
+run_test safari91
 
 # run_test iphone5 # fails weirdly, check later
 # run_test android4
