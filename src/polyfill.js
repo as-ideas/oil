8699470fb3459b69d1c82678027b1b8df9bf1e50
Dominique Klevinghaus <adk@codekommando.de>
2018-06-21 19:43:35 +0200

    Only use absolute necessary polyfills

--- src/polyfill.js
+++ src/polyfill.js
@@ -5,15 +5,9 @@ export function generatePolyfills(){
   }
   require('core-js/modules/es6.object.assign');
   require('core-js/modules/es6.string.starts-with');
-  require('core-js/modules/es6.array.from');
-  require('core-js/modules/es6.array.slice');
-  require('core-js/modules/es6.array.sort');
-  require('core-js/modules/es6.array.fill');
   require('core-js/modules/es6.array.iterator');
   require('core-js/modules/es6.promise');
-  require('core-js/modules/es7.array.includes');
   require('core-js/modules/es7.object.values');
-  require('core-js/modules/es7.object.entries');
   require('core-js/modules/es7.promise.finally');
   require('core-js/modules/es7.promise.try');
 
