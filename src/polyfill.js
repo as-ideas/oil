51be4212eb214c939f5bf2f6726f6d8d29c7edff
Dominique Klevinghaus <adk@codekommando.de>
2018-06-21 15:41:46 +0200

    Include regenerator-runtime

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,29 +1,28 @@
-
-require('core-js/modules/es6.object.assign');
-
-require('core-js/modules/es6.string.includes');
-require('core-js/modules/es6.string.starts-with');
-require('core-js/modules/es6.string.sub');
-require('core-js/modules/es6.string.sup');
-
-require('core-js/modules/es6.array.from');
-require('core-js/modules/es6.array.slice');
-require('core-js/modules/es6.array.sort');
-require('core-js/modules/es6.array.fill');
-require('core-js/modules/es6.array.iterator');
-
-require('core-js/modules/es6.promise');
-require('core-js/modules/es7.array.includes');
-require('core-js/modules/es7.object.values');
-require('core-js/modules/es7.object.entries');
-
-require('core-js/modules/es7.promise.finally');
-require('core-js/modules/es7.promise.try');
-
-  
+/* eslint-disable global-require */
 export function generatePolyfills(){
-  // eslint-disable-next-line global-require
-  return require('core-js/modules/_core');
+  if(window.Promise) {
+    return;
+  }
+  require('core-js/modules/es6.object.assign');
+  require('core-js/modules/es6.string.includes');
+  require('core-js/modules/es6.string.starts-with');
+  require('core-js/modules/es6.string.sub');
+  require('core-js/modules/es6.string.sup');
+  require('core-js/modules/es6.array.from');
+  require('core-js/modules/es6.array.slice');
+  require('core-js/modules/es6.array.sort');
+  require('core-js/modules/es6.array.fill');
+  require('core-js/modules/es6.array.iterator');
+  require('core-js/modules/es6.promise');
+  require('core-js/modules/es7.array.includes');
+  require('core-js/modules/es7.object.values');
+  require('core-js/modules/es7.object.entries');
+  require('core-js/modules/es7.promise.finally');
+  require('core-js/modules/es7.promise.try');
+
+  require('core-js/modules/_core');
+
+  require('regenerator-runtime/runtime');
 }
 
 export default generatePolyfills();
