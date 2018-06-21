ecfc6e2a6ceaebc707dec7a5a71840db8352317b
Dominique Klevinghaus <adk@codekommando.de>
2018-06-21 14:25:10 +0200

    still works

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,51 +1,92 @@
+require('core-js/modules/es6.symbol');
+require('core-js/modules/es6.object.create');
+require('core-js/modules/es6.object.define-property');
+require('core-js/modules/es6.object.define-properties');
+require('core-js/modules/es6.object.get-own-property-descriptor');
+require('core-js/modules/es6.object.get-prototype-of');
+require('core-js/modules/es6.object.keys');
+require('core-js/modules/es6.object.get-own-property-names');
+require('core-js/modules/es6.object.freeze');
+require('core-js/modules/es6.object.seal');
+require('core-js/modules/es6.object.prevent-extensions');
+require('core-js/modules/es6.object.is-frozen');
+require('core-js/modules/es6.object.is-sealed');
+require('core-js/modules/es6.object.is-extensible');
+require('core-js/modules/es6.object.assign');
+
+
+require('core-js/modules/es6.string.from-code-point');
+require('core-js/modules/es6.string.raw');
+require('core-js/modules/es6.string.trim');
+require('core-js/modules/es6.string.iterator');
+require('core-js/modules/es6.string.code-point-at');
+require('core-js/modules/es6.string.ends-with');
+require('core-js/modules/es6.string.includes');
+require('core-js/modules/es6.string.repeat');
+require('core-js/modules/es6.string.starts-with');
+require('core-js/modules/es6.string.link');
+require('core-js/modules/es6.string.small');
+require('core-js/modules/es6.string.strike');
+require('core-js/modules/es6.string.sub');
+require('core-js/modules/es6.string.sup');
+
+
+
+
+
+require('core-js/modules/es6.array.is-array');
+require('core-js/modules/es6.array.from');
+require('core-js/modules/es6.array.of');
+require('core-js/modules/es6.array.join');
+require('core-js/modules/es6.array.slice');
+require('core-js/modules/es6.array.sort');
+require('core-js/modules/es6.array.for-each');
+require('core-js/modules/es6.array.map');
+require('core-js/modules/es6.array.filter');
+require('core-js/modules/es6.array.some');
+require('core-js/modules/es6.array.every');
+require('core-js/modules/es6.array.reduce');
+require('core-js/modules/es6.array.reduce-right');
+require('core-js/modules/es6.array.index-of');
+require('core-js/modules/es6.array.last-index-of');
+require('core-js/modules/es6.array.copy-within');
+require('core-js/modules/es6.array.fill');
+require('core-js/modules/es6.array.find');
+require('core-js/modules/es6.array.find-index');
+require('core-js/modules/es6.array.species');
+require('core-js/modules/es6.array.iterator');
+
+require('core-js/modules/es6.promise');
+require('core-js/modules/es6.map');
+require('core-js/modules/es6.set');
+require('core-js/modules/es6.weak-map');
+require('core-js/modules/es6.weak-set');
+require('core-js/modules/es7.array.includes');
+require('core-js/modules/es7.array.flat-map');
+require('core-js/modules/es7.array.flatten');
+require('core-js/modules/es7.string.at');
+require('core-js/modules/es7.string.pad-start');
+require('core-js/modules/es7.string.pad-end');
+require('core-js/modules/es7.string.trim-left');
+require('core-js/modules/es7.string.trim-right');
+require('core-js/modules/es7.string.match-all');
+require('core-js/modules/es7.symbol.async-iterator');
+require('core-js/modules/es7.symbol.observable');
+require('core-js/modules/es7.object.get-own-property-descriptors');
+require('core-js/modules/es7.object.values');
+require('core-js/modules/es7.object.entries');
+require('core-js/modules/es7.object.define-getter');
+require('core-js/modules/es7.object.define-setter');
+require('core-js/modules/es7.object.lookup-getter');
+require('core-js/modules/es7.object.lookup-setter');
+
+require('core-js/modules/es7.promise.finally');
+require('core-js/modules/es7.promise.try');
+
+  
 export function generatePolyfills(){
-  if(typeof window === 'undefined' || !window.Promise) {
-    // eslint-disable-next-line global-require
-    window.Promise = require('promise-polyfill').default;
-  }
-
   // eslint-disable-next-line global-require
-  require('es6-object-assign/auto');
-
-  if (!Object.values) {
-    // eslint-disable-next-line global-require
-    Object.values = require('core-js/fn/object/values');
-  }
-
-  if (!Array.prototype.fill) {
-    // eslint-disable-next-line no-extend-native
-    Object.defineProperty(Array.prototype, 'fill', {
-      value: function(value) {
-
-        if (this === null) {
-          throw new TypeError('this is null or not defined');
-        }
-
-        let O = Object(this);
-        let len = O.length >>> 0;
-        let start = arguments[1];
-        let relativeStart = start >> 0;
-        let k = relativeStart < 0 ?
-          Math.max(len + relativeStart, 0) :
-          Math.min(relativeStart, len);
-
-        let end = arguments[2];
-        let relativeEnd = end === undefined ?
-          len : end >> 0;
-
-        let final = relativeEnd < 0 ?
-          Math.max(len + relativeEnd, 0) :
-          Math.min(relativeEnd, len);
-
-        while (k < final) {
-          O[k] = value;
-          k++;
-        }
-
-        return O;
-      }
-    });
-  }
+  return require('core-js/modules/_core');
 }
 
 export default generatePolyfills();
