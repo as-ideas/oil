c74beb8873034421883b070071c0184724701381
Dominique Klevinghaus <adk@codekommando.de>
2018-06-21 14:36:48 +0200

    Still going

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,8 +1,4 @@
-require('core-js/modules/es6.object.create');
-require('core-js/modules/es6.object.define-property');
-require('core-js/modules/es6.object.define-properties');
-require('core-js/modules/es6.object.keys');
-require('core-js/modules/es6.object.get-own-property-names');
+
 require('core-js/modules/es6.object.assign');
 
 require('core-js/modules/es6.string.includes');
@@ -11,14 +7,9 @@ require('core-js/modules/es6.string.sub');
 require('core-js/modules/es6.string.sup');
 
 require('core-js/modules/es6.array.from');
-require('core-js/modules/es6.array.join');
 require('core-js/modules/es6.array.slice');
 require('core-js/modules/es6.array.sort');
-require('core-js/modules/es6.array.for-each');
-require('core-js/modules/es6.array.map');
 require('core-js/modules/es6.array.fill');
-require('core-js/modules/es6.array.find');
-require('core-js/modules/es6.array.species');
 require('core-js/modules/es6.array.iterator');
 
 require('core-js/modules/es6.promise');
