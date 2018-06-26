a855167e56dc492e4f5fec3f9b45c475519095e2
Dominique Klevinghaus <adk@codekommando.de>
2018-06-26 14:46:28 +0200

    Make sure custom purposes aren't there usually

--- test/e2e/advanced_settings_custom_purposes_test.js
+++ test/e2e/advanced_settings_custom_purposes_test.js
@@ -11,17 +11,29 @@ module.exports = {
     browser
       .url(browser.globals.launch_url_host1 + 'demos/advanced-settings-custom-purposes.html')
       .deleteCookies();
+  },
 
+  'Shows five purposes by default': function (browser) {
+    browser
+      .url(browser.globals.launch_url_host1 + 'demos/advanced-settings.html')
+      .useCss()
+      .waitForElementVisible('body', 1000, false)
+      .useXpath().waitForElementVisible(OIL_YES_BUTTON, 2000, false)
+      .click(OIL_ADVANCED_SETTINGS)
+      .pause(200)
+      .waitForElementVisible(OIL_ADVANCED_SETTINGS_WRAPPER, 1000, false)
+      .pause(100)
+      .waitForElementNotPresent(OIL_ADVANCED_SETTINGS_CUSTOM_PURPOSE_HEADER, 1000, false)
+      .end();
+  },
+
+  'Displays custom purposes in list': function (browser) {
     browser
       .url(browser.globals.launch_url_host1 + 'demos/advanced-settings-custom-purposes.html')
       .useCss()
       .waitForElementVisible('body', 1000, false)
       .useXpath()
-      .waitForElementVisible(OIL_YES_BUTTON, 3000, false);
-  },
-
-  'Displays custom purposes in list': function (browser) {
-    browser
+      .waitForElementVisible(OIL_YES_BUTTON, 2000, false)
       .click(OIL_ADVANCED_SETTINGS)
       .pause(200)
       .waitForElementVisible(OIL_ADVANCED_SETTINGS_WRAPPER, 1000, false)
@@ -31,7 +43,7 @@ module.exports = {
       // FIXME selector fails in IE10
       // .assert.containsText(OIL_ADVANCED_SETTINGS_CUSTOM_PURPOSE_HEADER,'Custom Purpose 1')
       .pause(100)
-      .end()
+      .end();
   }
 
 };
