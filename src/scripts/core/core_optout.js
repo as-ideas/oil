f671985218ac1f536972cf50e5a9a5350fc850fb
Alexander Glintschert <alexander.glintschert@asideas.de>
2018-06-26 14:26:58 +0200

    OIL-89: send event for opt-out

--- src/scripts/core/core_optout.js
+++ src/scripts/core/core_optout.js
@@ -1,6 +1,8 @@
-import {deActivatePowerOptIn} from './core_poi.js';
-import {logInfo} from './core_log.js';
-import {removeSubscriberCookies} from './core_cookies.js';
+import { deActivatePowerOptIn } from './core_poi.js';
+import { logInfo } from './core_log.js';
+import { removeSubscriberCookies } from './core_cookies.js';
+import { sendEventToHostSite } from './core_utils';
+import { EVENT_NAME_OPT_OUT } from './core_constants';
 
 /**
  * Opt-Out Handler
@@ -13,5 +15,5 @@ export function handleOptOut() {
   // Update Oil cookie
   removeSubscriberCookies();
   // delete POI too if exists
-  deActivatePowerOptIn();
+  deActivatePowerOptIn().then(() => sendEventToHostSite(EVENT_NAME_OPT_OUT));
 }
