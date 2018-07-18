a83a950e7839afb6d254d3eba82df1df36bce72b
Alexander Glintschert <alexander.glintschert@asideas.de>
2018-07-18 12:43:26 +0200

    no ticket: fix waitsForAndRuns() helper function (do not check every millisecond!)

--- test/test-utils/utils_wait.js
+++ test/test-utils/utils_wait.js
@@ -1,3 +1,5 @@
+const REPEAT_INTERVAL = 10;
+
 export function waitsForAndRuns(escapeFunction, runFunction, escapeTime) {
   // check the escapeFunction every millisecond so as soon as it is met we can escape the function
   let interval = setInterval(function () {
@@ -5,7 +7,7 @@ export function waitsForAndRuns(escapeFunction, runFunction, escapeTime) {
       clearMe();
       runFunction();
     }
-  }, 1);
+  }, escapeTime > REPEAT_INTERVAL ? Math.floor(escapeTime / REPEAT_INTERVAL) : 1);
 
   // in case we never reach the escapeFunction, we will time out
   // at the escapeTime
@@ -28,12 +30,12 @@ export function waitForElementToDisplay(selector, callback) {
 export function waitForElementsToDisplay(selectors, callback) {
   let all_exist = true;
   selectors.map((selector) => {
-    if(!document.querySelector(selector)) {
+    if (!document.querySelector(selector)) {
       all_exist = false;
     }
   });
 
-  if(all_exist) {
+  if (all_exist) {
     return callback();
   } else {
     setTimeout(() => {
