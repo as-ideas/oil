eea88f8e85b6d99bd8a96614b6bccb97c1800725
Alexander Glintschert <alexander.glintschert@asideas.de>
2018-08-01 14:09:42 +0200

    no ticket: increase timeouts for IE

--- test/e2e/optout_confirm_test.js
+++ test/e2e/optout_confirm_test.js
@@ -22,13 +22,13 @@ module.exports = {
     browser
       .url(browser.globals.launch_url_host1 + 'demos/advanced-settings-confirm-optout.html')
       .useXpath()
-      .waitForElementVisible(OIL_LAYER, 1000, false)
+      .waitForElementVisible(OIL_LAYER, 5000, false)
       .click(OIL_ADVANCED_SETTINGS)
-      .waitForElementVisible(OIL_ADVANCED_SETTINGS_WRAPPER, 1000, false)
-      .waitForElementPresent(OIL_DEFAULT_PURPOSE_SLIDER, 1000, false)
-      .useCss().waitForElementNotPresent(CHECKED_SLIDER, 500, false)
+      .waitForElementVisible(OIL_ADVANCED_SETTINGS_WRAPPER, 5000, false)
+      .waitForElementPresent(OIL_DEFAULT_PURPOSE_SLIDER, 5000, false)
+      .useCss().waitForElementNotPresent(CHECKED_SLIDER, 2500, false)
       .click(SLIDER)
-      .useCss().waitForElementPresent(CHECKED_SLIDER, 500, false)
+      .useCss().waitForElementPresent(CHECKED_SLIDER, 2500, false)
       .pause(100)
       .click(SLIDER)
       .useXpath();
@@ -38,63 +38,63 @@ module.exports = {
     browser
       .url(browser.globals.launch_url_host1 + 'demos/advanced-settings.html')
       .useXpath()
-      .waitForElementVisible(OIL_LAYER, 1000, false)
+      .waitForElementVisible(OIL_LAYER, 5000, false)
       .click(OIL_ADVANCED_SETTINGS)
-      .waitForElementPresent(OIL_DEFAULT_PURPOSE_SLIDER, 1000, false)
+      .waitForElementPresent(OIL_DEFAULT_PURPOSE_SLIDER, 5000, false)
       .click(OIL_DEFAULT_PURPOSE_SLIDER)
       .pause(100)
       .click(OIL_DEFAULT_PURPOSE_SLIDER)
       .pause(100)
-      .waitForElementNotPresent(OIL_OPTOUT_CONFIRM, 500)
+      .waitForElementNotPresent(OIL_OPTOUT_CONFIRM, 2500)
       .end()
   },
 
   'Optout layer should show when trying to deactivate a consent slider': function (browser) {
     browser
-      .waitForElementPresent(OIL_OPTOUT_CONFIRM, 1000, false)
+      .waitForElementPresent(OIL_OPTOUT_CONFIRM, 5000, false)
       .end()
   },
 
   'Optout layer should hide when pressing proceed button': function (browser) {
     browser
-      .waitForElementPresent(OIL_PROCEED_BUTTON, 1000, false)
+      .waitForElementPresent(OIL_PROCEED_BUTTON, 5000, false)
       .click(OIL_PROCEED_BUTTON)
-      .waitForElementNotPresent(OIL_OPTOUT_CONFIRM, 2000, false)
+      .waitForElementNotPresent(OIL_OPTOUT_CONFIRM, 5000, false)
       .end()
   },
 
   'Slider should get deactivated when proceed was clicked in modal': function (browser) {
     browser
-      .waitForElementPresent(OIL_PROCEED_BUTTON, 1000, false)
+      .waitForElementPresent(OIL_PROCEED_BUTTON, 5000, false)
       .click(OIL_PROCEED_BUTTON)
       .pause(200)
-      .useCss().waitForElementNotPresent(CHECKED_SLIDER, 500, false)
+      .useCss().waitForElementNotPresent(CHECKED_SLIDER, 2500, false)
       .end();
   },
 
   'Optout layer should hide when pressing cancel button': function (browser) {
     browser
-      .waitForElementPresent(OIL_CANCEL_BUTTON, 1000, false)
+      .waitForElementPresent(OIL_CANCEL_BUTTON, 5000, false)
       .click(OIL_CANCEL_BUTTON)
-      .waitForElementNotPresent(OIL_OPTOUT_CONFIRM, 2000, false)
+      .waitForElementNotPresent(OIL_OPTOUT_CONFIRM, 5000, false)
       .end();
   },
 
   'Slider should remain activated when cancel was clicked in modal': function (browser) {
     browser
-      .waitForElementPresent(OIL_CANCEL_BUTTON, 1000, false)
+      .waitForElementPresent(OIL_CANCEL_BUTTON, 5000, false)
       .click(OIL_CANCEL_BUTTON)
       .pause(100)
-      .useCss().waitForElementPresent(CHECKED_SLIDER, 500, false)
+      .useCss().waitForElementPresent(CHECKED_SLIDER, 2500, false)
       .end();
   },
 
   'Slider should remain activated when background area was clicked': function (browser) {
     browser
-      .waitForElementPresent(OIL_CANCEL_BUTTON, 1000, false)
+      .waitForElementPresent(OIL_CANCEL_BUTTON, 5000, false)
       .click(OIL_OPTOUT_CONFIRM)
       .pause(100)
-      .useCss().waitForElementPresent(CHECKED_SLIDER, 500, false)
+      .useCss().waitForElementPresent(CHECKED_SLIDER, 2500, false)
       .end();
   }
 
