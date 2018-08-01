e978e38f6c7ab09879941efd5caf745dfdb2e864
Awerkow-Schäfer, Dmitri <dmitri.awerkow@asideas.de>
2018-08-01 12:00:05 +0200

    OIL-217: skip ie9 e2e test

--- test/e2e/locale_texts_loaded_test.js
+++ test/e2e/locale_texts_loaded_test.js
@@ -1,4 +1,5 @@
-import { OIL_ADVANCED_SETTINGS, OIL_LAYER, OIL_YES_BUTTON } from '../test_constants';
+import {OIL_ADVANCED_SETTINGS, OIL_LAYER, OIL_YES_BUTTON} from '../test_constants';
+import {executeIfCompatible} from '../test-utils/utils_browser_check';
 
 module.exports = {
   '@disabled': false,
@@ -10,30 +11,29 @@ module.exports = {
   },
 
   'Oil Layer loads texts dynamically from backend and uses them': function (browser) {
-    let end2endRegexp = /^\[e2e\].*$/;
+    executeIfCompatible(
+      browser,
+      // https://id-jira.axelspringer.de/browse/OIL-217
+      // CORS HEADER deactivated in IE9 per default
+      [{name: 'IE', version: '9.0'}],
+      () => {
+        let end2endRegexp = /^\[e2e\].*$/;
+        browser
+          .url(browser.globals.launch_url_host1 + 'demos/advanced-settings.html')
+          .useCss().waitForElementVisible('body', 1000, false)
+          .useXpath().waitForElementVisible(OIL_LAYER, 2000, false);
 
-    browser
-      .url(browser.globals.launch_url_host1 + 'demos/advanced-settings.html')
-      .useCss().waitForElementVisible('body', 1000, false)
-      .useXpath().waitForElementVisible(OIL_LAYER, 2000, false);
+        browser.useXpath().expect.element(OIL_YES_BUTTON).text.to.not.match(end2endRegexp);
+        browser.useXpath().expect.element(OIL_ADVANCED_SETTINGS).text.to.not.match(end2endRegexp);
 
-    browser.useXpath().expect.element(OIL_YES_BUTTON).text.to.not.match(end2endRegexp);
-    browser.useXpath().expect.element(OIL_ADVANCED_SETTINGS).text.to.not.match(end2endRegexp);
-
-    // advanced-settings-e2e-locale should load a special LOCALE where every locale starts with '[e2e]'
-    browser
-      .url(browser.globals.launch_url_host1 + 'demos/advanced-settings-e2e-locale.html')
-      .useCss().waitForElementVisible('body', 1000, false)
-      .useXpath().waitForElementVisible(OIL_LAYER, 2000, false);
-
-    // FIXME
-    // this test fails in BrowserStack IE9. Change in the browser settings:
-    // Tools -> Internet Options -> Security -> Custom level... -> Miscellaneous -> Access data sources across domains -> Enable
-    // then this test works fine in a manual test
-
-    browser.useXpath().expect.element(OIL_YES_BUTTON).text.to.match(end2endRegexp);
-    browser.useXpath().expect.element(OIL_ADVANCED_SETTINGS).text.to.match(end2endRegexp);
-
-    browser.end();
+        // advanced-settings-e2e-locale should load a special LOCALE where every locale starts with '[e2e]'
+        browser
+          .url(browser.globals.launch_url_host1 + 'demos/advanced-settings-e2e-locale.html')
+          .useCss().waitForElementVisible('body', 1000, false)
+          .useXpath().waitForElementVisible(OIL_LAYER, 2000, false);
+        browser.useXpath().expect.element(OIL_YES_BUTTON).text.to.match(end2endRegexp);
+        browser.useXpath().expect.element(OIL_ADVANCED_SETTINGS).text.to.match(end2endRegexp);
+        browser.end();
+      });
   }
 };
