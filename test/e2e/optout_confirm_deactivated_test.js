6c7f3041de21990e98b3f72d1b959b3955f19729
Awerkow-Schäfer, Dmitri <dmitri.awerkow@asideas.de>
2018-08-01 15:25:16 +0200

    no-ticket: separate some test cases

---
+++ test/e2e/optout_confirm_deactivated_test.js
@@ -0,0 +1,31 @@
+import {
+  OIL_ADVANCED_SETTINGS,
+  OIL_DEFAULT_PURPOSE_SLIDER,
+  OIL_LAYER,
+  OIL_OPTOUT_CONFIRM
+} from '../test_constants';
+
+module.exports = {
+  '@disabled': false,
+
+  beforeEach: browser => {
+    browser.deleteCookies();
+  },
+
+  'Optout layer should remain hidden when require_optout_confirm not in config or false': function (browser) {
+    browser
+      .url(browser.globals.launch_url_host1 + 'demos/advanced-settings.html')
+      .useXpath()
+      .waitForElementVisible(OIL_LAYER, 5000, false)
+      .click(OIL_ADVANCED_SETTINGS)
+      .waitForElementPresent(OIL_DEFAULT_PURPOSE_SLIDER, 5000, false)
+      .click(OIL_DEFAULT_PURPOSE_SLIDER)
+      .pause(100)
+      .click(OIL_DEFAULT_PURPOSE_SLIDER)
+      .pause(100)
+      .waitForElementNotPresent(OIL_OPTOUT_CONFIRM, 2500)
+      .end()
+  },
+
+
+};
