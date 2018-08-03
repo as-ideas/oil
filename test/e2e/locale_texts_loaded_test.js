5b32ac3aadc91a9792215648613dd54a4dacf0f0
Awerkow-Schäfer, Dmitri <dmitri.awerkow@asideas.de>
2018-08-03 16:41:20 +0200

    no-ticket: optimize css for some old browser

--- test/e2e/locale_texts_loaded_test.js
+++ test/e2e/locale_texts_loaded_test.js
@@ -16,22 +16,23 @@ module.exports = {
         {name: 'IE', version: '9.0', reason: 'CORS HEADER deactivated in IE9 per default - see https://id-jira.axelspringer.de/browse/OIL-217.'}
       ],
       () => {
-        let end2endRegexp = /^\[e2e\].*$/;
+        let end2endRegexp = 'e2e';
+
         browser
           .url(browser.globals.launch_url_host1 + 'demos/advanced-settings.html')
           .useCss().waitForElementVisible('body', 1000, false)
           .useXpath().waitForElementVisible(OIL_LAYER, 2000, false);
 
-        browser.useXpath().expect.element(OIL_YES_BUTTON).text.to.not.match(end2endRegexp);
-        browser.useXpath().expect.element(OIL_ADVANCED_SETTINGS).text.to.not.match(end2endRegexp);
+        browser.useXpath().expect.element(OIL_YES_BUTTON).text.to.not.contain(end2endRegexp);
+        browser.useXpath().expect.element(OIL_ADVANCED_SETTINGS).text.to.not.contain(end2endRegexp);
 
         // advanced-settings-e2e-locale should load a special LOCALE where every locale starts with '[e2e]'
         browser
           .url(browser.globals.launch_url_host1 + 'demos/advanced-settings-e2e-locale.html')
           .useCss().waitForElementVisible('body', 1000, false)
           .useXpath().waitForElementVisible(OIL_LAYER, 2000, false);
-        browser.useXpath().expect.element(OIL_YES_BUTTON).text.to.match(end2endRegexp);
-        browser.useXpath().expect.element(OIL_ADVANCED_SETTINGS).text.to.match(end2endRegexp);
+        browser.useXpath().expect.element(OIL_YES_BUTTON).text.to.contain(end2endRegexp);
+        browser.useXpath().expect.element(OIL_ADVANCED_SETTINGS).text.to.contain(end2endRegexp);
         browser.end();
       });
   }
