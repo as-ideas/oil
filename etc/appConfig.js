7717cb54dc008f1bd2bf57919d90d25f0fb71e46
Dominique Klevinghaus <adk@codekommando.de>
2018-08-09 16:22:29 +0200

    Add poi-group-whitelist demo page

--- etc/appConfig.js
+++ etc/appConfig.js
@@ -181,6 +181,12 @@ module.exports = {
     chunksSortMode: 'dependency',
     inject: 'head'
   }, {
+    filename: 'demos/poi-group-whitelist.html',
+    template: path.resolve(sourcePath, 'demos', 'poi-group-whitelist.html'),
+    chunks: ['oilstub', 'oil'],
+    chunksSortMode: 'dependency',
+    inject: 'head'
+  }, {
     filename: 'demos/tealium-integration-test.html',
     template: path.resolve(sourcePath, 'demos', 'tealium-integration-test.html'),
     chunks: [],
