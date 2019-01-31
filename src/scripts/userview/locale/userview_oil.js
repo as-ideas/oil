21a704d3bf270cbfee52bb1cc6b1e1030eff8a0f
Sebastian Waschnick <barriwaschi@web.de>
2019-01-31 09:53:01 +0100

    OIL-297 FIXED Labels with Prefix label_cpc_purpose no loaded correctly (#228)

--- src/scripts/userview/locale/userview_oil.js
+++ src/scripts/userview/locale/userview_oil.js
@@ -12,50 +12,82 @@ export {
   oilWrapper
 } from '../userview_modal';
 
+/**
+ *
+ * Loads the locale information and fills missing texts
+ *
+ * @param callback function which gets called after locale is loaded
+ * @returns {*}
+ */
 export function locale(callback) {
-  let localeObject = getLocale();
-  const missingLabels = (localeObject && localeObject.texts) ? findMissingLabels(localeObject.texts) : [];
+  let localeObjectFromConfiguration = getLocale();
 
-  if (localeObject && localeObject.texts && missingLabels.length === 0) {
+  if (hasMissingLabels(localeObjectFromConfiguration)) {
     return callback(this);
   } else {
     const localeUrl = getLocaleUrl();
 
     if (localeUrl) {
       fetchJsonData(localeUrl)
-      .then(response => {
-        fillAndSetLocaleObject(response, localeObject, missingLabels);
-        return callback(this);
-      })
-      .catch(error => {
-        logError(`OIL backend returned error: ${error}. Falling back to default locale '${DEFAULT_LOCALE.localeId}', version ${DEFAULT_LOCALE.version}!`);
-        fillAndSetLocaleObject(DEFAULT_LOCALE, localeObject, missingLabels);
-        return callback(this);
-      });
+        .then(response => {
+          fillAndSetLocaleObject(response, localeObjectFromConfiguration);
+          return callback(this);
+        })
+        .catch(error => {
+          logError(`OIL backend returned error: ${error}. Falling back to default locale '${DEFAULT_LOCALE.localeId}', version ${DEFAULT_LOCALE.version}!`);
+          fillAndSetLocaleObject(DEFAULT_LOCALE, localeObjectFromConfiguration);
+
+          return callback(this);
+        });
     } else {
-      fillAndSetLocaleObject(DEFAULT_LOCALE, localeObject, missingLabels);
+      // HINT: For no localeUrl we enrich the configured locale with our default locale
+      fillAndSetLocaleObject(DEFAULT_LOCALE, localeObjectFromConfiguration);
       return callback(this);
     }
   }
 }
 
-function fillAndSetLocaleObject(sourceObject, targetObject, missingLabels) {
+function hasMissingLabels(localeObjectFromConfiguration) {
+  const missingLabels = (localeObjectFromConfiguration && localeObjectFromConfiguration.texts) ? findMissingLabels(localeObjectFromConfiguration.texts) : [];
+  return localeObjectFromConfiguration && localeObjectFromConfiguration.texts && missingLabels.length === 0;
+}
+
+/**
+ * Gets a source and a target and adds all missing labels from the source to the target
+ *
+ * @param sourceObject default object
+ * @param targetObject object with missing labels
+ */
+function fillAndSetLocaleObject(sourceObject, targetObject) {
   if (!targetObject || !targetObject.texts) {
+    console.error('ªfillAndSetLocaleObject: use SOURCEOBJECT, target was', targetObject);
     setLocale(sourceObject);
     return;
   }
 
-  missingLabels.forEach((label) => {
-    if(!sourceObject.texts[label]){
-      logWarn(`${label} missing from locale config.`);
-    } else {
-      targetObject.texts[label] = sourceObject.texts[label];
+  for (let key in sourceObject.texts) {
+    if (!targetObject.texts[key]) {
+      targetObject.texts[key] = sourceObject.texts[key];
     }
-  });
+  }
+
+  // Check for missing keys against default configuration
+  for (let key in DEFAULT_LOCALE.texts) {
+    if (!targetObject.texts[key]) {
+      logWarn(`${key} missing from locale config.`);
+    }
+  }
 
   setLocale(targetObject);
 }
 
+/**
+ * Determines which label-keys are mandatory.
+ * Currently all labels starting with "label_cpc_purpose" are optional.
+ *
+ * @param locale
+ * @returns {any[]} An array with all keys of missing labels
+ */
 function findMissingLabels(locale) {
   const requiredLabels = Object.values(OIL_LABELS).filter(key => !key.startsWith(OPTIONAL_LABEL_PREFIX));
   return requiredLabels.filter(key => !locale[key]);
