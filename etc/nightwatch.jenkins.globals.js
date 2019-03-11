c306fb7d45dfa76e2b2865c8f384760b6573f60d
subsend <subsend@googlemail.com>
2019-03-11 14:26:08 +0100

    remove html reporter from nightwatch tests AGASSI-8

--- etc/nightwatch.jenkins.globals.js
+++ etc/nightwatch.jenkins.globals.js
@@ -1,9 +1,3 @@
-const HtmlReporter = require('nightwatch-html-reporter');
-let reporter = new HtmlReporter({
-  uniqueFilename: true,
-  reportsDirectory: __dirname + '/../target/reports'
-});
-
 module.exports = {
   openBrowser: false,
   'launch_url_host1': 'https://oil-integration-host1.herokuapp.com/',
