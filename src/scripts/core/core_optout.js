5a605b06a0f238e8476c17bbcac43a1193c7dfe3
Alexander Glintschert <alexander.glintschert@asideas.de>
2019-04-30 13:49:53 +0200

    send consent to custom vendors (and execute commands for IAB vendors) if opt out is invoked OIL-320

--- src/scripts/core/core_optout.js
+++ src/scripts/core/core_optout.js
@@ -4,18 +4,16 @@ import { removeSubscriberCookies } from './core_cookies.js';
 import { sendEventToHostSite } from './core_utils';
 import { EVENT_NAME_OPT_OUT } from './core_constants';
 import { manageDomElementActivation } from './core_tag_management';
+import { executeCommandCollection } from './core_command_collection';
+import { sendConsentInformationToCustomVendors } from './core_custom_vendors';
 
-/**
- * Opt-Out Handler
- *
- * @return none
- */
-// FIXME write test
 export function handleOptOut() {
   logInfo('OptOut Received.');
   // Update Oil cookie
   removeSubscriberCookies();
   // delete POI too if exists
   deActivatePowerOptIn().then(() => sendEventToHostSite(EVENT_NAME_OPT_OUT));
+  executeCommandCollection();
+  sendConsentInformationToCustomVendors().then(() => logInfo('Consent information sending to custom vendors after opt-out is done!'));
   manageDomElementActivation();
 }
