da95f6c5cdca66c77c4022a5c30dcc5ba1446890
Alexander Glintschert <alexander.glintschert@asideas.de>
2019-05-13 09:31:02 +0200

    deactivate custom vendor e2e tests for IE9 (no default CORS support) OIL-339

--- test/e2e/cookie_preference_center_custom_vendors_test.js
+++ test/e2e/cookie_preference_center_custom_vendors_test.js
@@ -1,10 +1,5 @@
-import {
-  OIL_ADVANCED_SETTINGS,
-  OIL_ADVANCED_SETTINGS_WRAPPER,
-  OIL_CUSTOM_VENDORS_HEADLINE,
-  OIL_CUSTOM_VENDORS_LIST,
-  OIL_YES_BUTTON
-} from '../test_constants';
+import { OIL_ADVANCED_SETTINGS, OIL_ADVANCED_SETTINGS_WRAPPER, OIL_CUSTOM_VENDORS_HEADLINE, OIL_CUSTOM_VENDORS_LIST, OIL_YES_BUTTON } from '../test_constants';
+import { executeIfCompatible } from '../test-utils/utils_browser_check';
 
 module.exports = {
   '@disabled': false,
@@ -14,20 +9,27 @@ module.exports = {
   },
 
   'Displays custom vendors in list': function (browser) {
-    browser
-      .url(browser.globals.launch_url_host1 + 'demos/cpc-custom-vendors.html')
-      .useXpath()
-      .waitForElementPresent(OIL_YES_BUTTON, 15000, false)
-      .click(OIL_ADVANCED_SETTINGS)
-      .pause(200)
-      .waitForElementPresent(OIL_ADVANCED_SETTINGS_WRAPPER, 2000, false)
-      .pause(100)
-      .waitForElementPresent(OIL_CUSTOM_VENDORS_HEADLINE, 100, false)
-      .waitForElementPresent(OIL_CUSTOM_VENDORS_LIST, 100, false)
-      .assert.containsText(OIL_CUSTOM_VENDORS_LIST,'CUSTOM_VENDOR_1')
-      .assert.containsText(OIL_CUSTOM_VENDORS_LIST,'CUSTOM_VENDOR_2')
-      .pause(100)
-      .end();
+    executeIfCompatible(
+      browser,
+      [
+        {name: 'IE', version: '9.0', reason: 'CORS HEADER deactivated in IE9 per default - see https://id-jira.axelspringer.de/browse/OIL-217.'}
+      ],
+      () => {
+        browser
+          .url(browser.globals.launch_url_host1 + 'demos/cpc-custom-vendors.html')
+          .useXpath()
+          .waitForElementPresent(OIL_YES_BUTTON, 15000, false)
+          .click(OIL_ADVANCED_SETTINGS)
+          .pause(200)
+          .waitForElementPresent(OIL_ADVANCED_SETTINGS_WRAPPER, 2000, false)
+          .pause(100)
+          .waitForElementPresent(OIL_CUSTOM_VENDORS_HEADLINE, 100, false)
+          .waitForElementPresent(OIL_CUSTOM_VENDORS_LIST, 100, false)
+          .assert.containsText(OIL_CUSTOM_VENDORS_LIST, 'CUSTOM_VENDOR_1')
+          .assert.containsText(OIL_CUSTOM_VENDORS_LIST, 'CUSTOM_VENDOR_2')
+          .pause(100)
+          .end();
+      })
   }
 
 };
--- test/e2e/custom_vendors_notification_test.js
+++ test/e2e/custom_vendors_notification_test.js
@@ -1,4 +1,5 @@
 import { CUSTOM_VENDOR_ONE_ELEMENT, CUSTOM_VENDOR_TWO_ELEMENT, OIL_YES_BUTTON } from '../test_constants';
+import { executeIfCompatible } from '../test-utils/utils_browser_check';
 
 module.exports = {
   '@disabled': false,
@@ -8,22 +9,29 @@ module.exports = {
   },
 
   'Notifies custom vendors in list if consent is given': function (browser) {
-    browser
-      .url(browser.globals.launch_url_host1 + 'demos/custom-vendors-notification.html')
-      .useXpath()
-      .pause(500)
-      .waitForElementPresent(CUSTOM_VENDOR_ONE_ELEMENT)
-      .assert.containsText(CUSTOM_VENDOR_ONE_ELEMENT, 'Custom Vendor 1 was opted out')
-      .waitForElementPresent(CUSTOM_VENDOR_TWO_ELEMENT)
-      .assert.containsText(CUSTOM_VENDOR_TWO_ELEMENT, 'Custom Vendor 2 was opted out')
-      .waitForElementPresent(OIL_YES_BUTTON, 15000, false)
-      .click(OIL_YES_BUTTON)
-      .pause(500)
-      .waitForElementPresent(CUSTOM_VENDOR_ONE_ELEMENT)
-      .assert.containsText(CUSTOM_VENDOR_ONE_ELEMENT, 'Custom Vendor 1 was opted in')
-      .waitForElementPresent(CUSTOM_VENDOR_TWO_ELEMENT)
-      .assert.containsText(CUSTOM_VENDOR_TWO_ELEMENT, 'Custom Vendor 2 was opted in')
-      .end();
+    executeIfCompatible(
+      browser,
+      [
+        {name: 'IE', version: '9.0', reason: 'CORS HEADER deactivated in IE9 per default - see https://id-jira.axelspringer.de/browse/OIL-217.'}
+      ],
+      () => {
+        browser
+          .url(browser.globals.launch_url_host1 + 'demos/custom-vendors-notification.html')
+          .useXpath()
+          .pause(500)
+          .waitForElementPresent(CUSTOM_VENDOR_ONE_ELEMENT)
+          .assert.containsText(CUSTOM_VENDOR_ONE_ELEMENT, 'Custom Vendor 1 was opted out')
+          .waitForElementPresent(CUSTOM_VENDOR_TWO_ELEMENT)
+          .assert.containsText(CUSTOM_VENDOR_TWO_ELEMENT, 'Custom Vendor 2 was opted out')
+          .waitForElementPresent(OIL_YES_BUTTON, 15000, false)
+          .click(OIL_YES_BUTTON)
+          .pause(500)
+          .waitForElementPresent(CUSTOM_VENDOR_ONE_ELEMENT)
+          .assert.containsText(CUSTOM_VENDOR_ONE_ELEMENT, 'Custom Vendor 1 was opted in')
+          .waitForElementPresent(CUSTOM_VENDOR_TWO_ELEMENT)
+          .assert.containsText(CUSTOM_VENDOR_TWO_ELEMENT, 'Custom Vendor 2 was opted in')
+          .end();
+      })
   }
 
 };
